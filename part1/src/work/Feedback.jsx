6539558c1e03782b444633e1e0b4853d7d53085e
Jacob Phillips <jacob.phillips8905@gmail.com>
2021-08-29 14:14:18 -0500

    made button statistics and reset

--- part1/src/work/Feedback.jsx
+++ part1/src/work/Feedback.jsx
@@ -1,31 +1,44 @@
 import {React, useState} from 'react'
+import _ from 'lodash'
 
 const Feedback = () => {
-    const [counts, setCounts] = useState({
-        good: 0, neutral: 0, bad: 0
-    })
+    const originalState = {good: 0, neutral: 0, bad: 0, total: 0}
+    const [counts, setCounts] = useState(_.clone(originalState))
 
     const handleGoodClick = () => {
-            setCounts({...counts, good: counts.good + 1})
+            setCounts({...counts, good: counts.good + 1, total: counts.total + 1})
     }
 
     const handleNeutralClick = () => {
-            setCounts({...counts, neutral: counts.neutral + 1})
+            setCounts({...counts, neutral: counts.neutral + 1, total: counts.total + 1})
     }
 
     const handleBadClick = () => {
-            setCounts({...counts, bad: counts.bad + 1})
+            setCounts({...counts, bad: counts.bad + 1, total: counts.total + 1})
     }
-
+    const handleResetClick = () => {
+            setCounts(_.clone(originalState))
+    }
+    const stats = () => {
+        console.log("calling stats with count: ", counts)
+        console.log(Object.entries(counts))
+        return <ul>{Object.entries(counts).map((k, ind) => k[0] !== 'total' && (<li key={ind}>{k[0]}: {(100 *k[1] / counts.total).toFixed(2)}%</li>))}</ul>
+    }
+    const displayStats = (counts.total > 0) &&  (<div>
+                            <h1>Statistics:</h1>
+                            {stats()}
+                        </div>)
     return (
         <div>
             <h1>Give Feedback!</h1>
             <button onClick={handleGoodClick}>good :)</button>
             <button onClick={handleNeutralClick}>neutral :|</button>
             <button onClick={handleBadClick}>bad :(</button>
-            <h1>Statistics:</h1>
+            <h1>Counts:</h1>
             <ul>{Object.entries(counts).map((k, ind) => <li key={ind}>{k[0]}: {k[1]}</li>)}</ul>
-            <h1>that's it</h1>
+            {displayStats}
+            <button onClick={handleResetClick}>RESET</button>
+
         </div>
 
     )
