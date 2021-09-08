2e8978bc002373b611c11211fab842e13ce76d0d
Jacob Phillips <jacob.phillips8905@gmail.com>
2021-09-08 14:04:04 -0400

    adding search functionality

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,30 +1,64 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([{ name: 'Arto Hellas' }]) 
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ searchPrefix, setSearchPrefix ] = useState('')
 
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault()
-    const newPerson = {
-        name: newName
+    if (newName.length === 0 || newName === " ") {
+        setNewName('')
+        setNewNumber('')
+        return
     }
-    setPersons(persons.concat(newPerson))
+    const newPerson = {
+        name: newName,
+        number: newNumber
+    }
+
+    if (!persons.some(p => p.name === newName && p.number === newNumber)) {
+        setPersons(persons.concat(newPerson))
+    } else {
+        alert(`${newName} is already in the phonebook!`)
+    }
     setNewName('')
+    setNewNumber('')
   }
+
   const handleNameChange = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleSearchPrefixChange = (event) => {
+      setSearchPrefix(event.target.value.toLowerCase())
+  }
+
+  const peopleToShow = persons.filter(p => p.name.toLowerCase().startsWith(searchPrefix))
+
   return (
     <div>
-      <h2>Submit name!</h2>
-      <form onSubmit={addName}>
-          name: <input value={newName} onChange={handleNameChange}/>
-          <button type="submit">submit</button>
+      <h1>Phonebook!</h1>
+      <form>
+          filter for names beginning with <input value={searchPrefix} onChange={handleSearchPrefixChange}/>
       </form>
-      <h2>Phonebook</h2>
-        <ul>{persons.map((p, ind) => <li key={ind}>{p.name}</li>)}</ul>
+      <ul>{peopleToShow.map((p, ind) => <li key={ind}>{p.name}: {p.number}</li>)}</ul>
+      <h3>Add a new name and number!</h3>
+      <form onSubmit={addPerson}>
+          <div>name:   <input value={newName} onChange={handleNameChange}/></div>
+          <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
+          <div><button type="submit">submit</button></div>
+      </form>
     </div>
   )
 }
